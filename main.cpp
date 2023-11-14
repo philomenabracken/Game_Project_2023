317531b7e1ef48be1eff86494034e46cc204be51
philomena <philomenabracken94@gmail.com>
2023-11-14 22:02:09 +0000

    first commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include "raylib.h"
+
+
+int main() {
+const int screenWidth = 800;
+ const int screenHeight = 450;
+
+   InitWindow(screenWidth, screenHeight, "Fire Ball Game ");
+
+  SetTargetFPS(80);
+
+    while (!WindowShouldClose() /*WindowShouldClose returns true if esc is clicked and closes the window*/){
+
+
+    }
+  CloseWindow();
+    return 0;
+}
+
